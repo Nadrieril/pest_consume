f973cba4295a3ce9a5466e7cf5a6bc37e9a194c5
Nadrieril <nadrieril@gmail.com>
2019-09-19 17:24:13 +0200

    Document custom user data

--- pest_consume/src/lib.rs
+++ pest_consume/src/lib.rs
@@ -155,13 +155,67 @@
 //!
 //! # Advanced features
 //!
+//! ## Precedence climbing
+//!
 //! TODO
 //!
-//! - user data
-//! - precedence climbing
-//! - rule aliasing
-//! - rule shortcutting
-//! - match_nodes outside of a parser impl
+//! ## Passing user data through the parser
+//!
+//! Sometimes, you may want to pass some data to the consuming methods.
+//! You could want access to configuration data, or to a store for
+//! [string interning](https://en.wikipedia.org/wiki/String_interning).
+//!
+//! This is easy to do: by using [`Parser::parse_with_userdata`] instead of [`Parser::parse`],
+//! you can provide some data that will be stored in the returned [`Nodes`] value, and accessible at any point
+//! during parsing via [`Node::user_data`].
+//!
+//! The type of the user data is the second type parameter in the types of `Node<'i, Rule, Data>` and `Nodes<'i, Rule, Data>`.
+//! The data needs to be `Clone`, and will be cloned often so it should be cheap to clone.
+//! A common usage is to have this data be a reference, which are free to clone.
+//!
+//! If you need mutable access to some data, use [`Cell`] or [`RefCell`].
+//!
+//! ```ignore
+//! struct AppSettings { ... }
+//!
+//! // We changed the type alias to include the type of the user data.
+//! type Node<'i, 'a> = pest_consume::Node<'i, Rule, &'a AppSettings>;
+//!
+//! fn parse_with_settings(
+//!     input_str: &str,
+//!     settings: &AppSettings
+//! ) -> Result<Vec<Vec<f64>>> {
+//!     // Parse the input into `Nodes`, passing the provided settings along
+//!     let inputs = CSVParser::parse_with_userdata(
+//!         Rule::file,
+//!         input_str,
+//!         settings
+//!     )?;
+//!     let input = inputs.single()?;
+//!     CSVParser::file(input)
+//! }
+//!
+//! #[pest_consume::parser]
+//! impl CSVParser {
+//!     fn field(input: Node) -> Result<f64> {
+//!         // The settings can be retrieved from any Node.
+//!         let settings = input.user_data();
+//!         if settings.do_the_thing {
+//!             ...
+//!         }
+//!         ...
+//!     }
+//!     ...
+//! }
+//! ```
+//!
+//! # Rule aliasing
+//!
+//! TODO
+//!
+//! # Rule shortcutting
+//!
+//! TODO
 //!
 //! # Compatibility
 //!
@@ -194,8 +248,12 @@
 //! [`Node`]: struct.Node.html
 //! [`Node::as_str`]: struct.Node.html#method.as_str
 //! [`Node::error`]: struct.Node.html#method.as_error
+//! [`Node::user_data`]: struct.Node.html#method.user_data
 //! [`Parser`]: trait.Parser.html
 //! [`Parser::parse`]: trait.Parser.html#method.parse
+//! [`Parser::parse_with_userdata`]: trait.Parser.html#method.parse_with_userdata
+//! [`Cell`]: https://doc.rust-lang.org/std/cell/struct.Cell.html
+//! [`RefCell`]: https://doc.rust-lang.org/std/cell/struct.RefCell.html
 //! [pest]: https://pest.rs
 //! [examples]: https://github.com/Nadrieril/pest_consume/tree/master/pest_consume/examples
 //! [dhall-rust-parser]: https://github.com/Nadrieril/dhall-rust/blob/master/dhall_syntax/src/parser.rs
@@ -205,7 +263,7 @@ use pest::Parser as PestParser;
 use pest::RuleType;
 pub use pest_derive::Parser;
 
-// TODO: better doc
+// TODO: better doc, match_nodes outside of a parser impl
 /// Pattern-match on [`Nodes`] with familiar syntax and strong types..
 ///
 /// See [examples] and [crate-level documentation][`pest_consume`] for usage.
@@ -423,6 +481,12 @@ mod node {
             )
         }
 
+        pub fn user_data(&self) -> &D {
+            &self.user_data
+        }
+        pub fn into_user_data(self) -> D {
+            self.user_data
+        }
         pub fn as_pairs(&self) -> &Pairs<'i, R> {
             &self.pairs
         }
